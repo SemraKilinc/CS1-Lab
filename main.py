017c2b04784fcf5407b24078d4b75408e73fcc5d
SemraKilinc <60199182+SemraKilinc@users.noreply.github.com>
2020-01-29 15:28:15 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+#ex1
+print ("ex1.py")
+print ("Hello World")
+print ("Hello Again")
+print ("I like typing this.")
+print ("This is fun.")
+print ("Yay! Printing.")
+print ("I'd much rather you 'not'.")
+print ('I "said" do not touch this.')
+
+#ex2
+print("ex2.py")
+# A comment, this is so you can read your program later.
+#Anything after the # is ignored by python.
+
+print ("I could have code like this.") # and the comment after is ignored.
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "This won't run"
+
+print("This will run.")
+
+#ex3
+print("ex3.py")
+print("I will now count my chickens:")
+print ("Hens", 25+30/6)
+print ("Roosters",100-25*3%4)
+print("Now I will count the eggs.")
+print (3+2+1-5+4%2-1/4+6)
+print ("Is is true that 3+2<5-7?")
+print (3+2<5-7)
+print ("What is 3+2?",3+2)
+print ("What is 5-7?",5-7)
+print ("Oh, that's why it's False.")
+print ("How about some more.")
+print ("Is it greater?",5>-2)
+print ("Is it greater or equal?",5>=-2)
+print ("Is it less or equal?",5<=-2)
