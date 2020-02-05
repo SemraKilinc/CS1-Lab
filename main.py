836dfafd70eeac6717f180f0184cd05578c929b9
SemraKilinc <60199182+SemraKilinc@users.noreply.github.com>
2020-02-05 15:14:23 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -54,3 +54,44 @@ print("There will be",cars_not_driven,"empty cars today.")
 print("We can transport",carpool_capacity,"people today.")
 print ("We have",passengers,"to carpool today.")
 print ("We need to put about",average_passengers_per_car,"in each car.")
+
+
+#ex5
+my_name='Zed A. Shaw'
+my_age=35 # not a lie
+my_height=74 # inches
+my_weight=180 # lbs
+my_eyes='Blue'
+my_teeth='White'
+my_hair='Brown'
+
+print("Let's talk about %s." % my_name)
+print("He's %d inches tall."% my_height)
+print("He's %d pounds heavy."% my_weight)
+print("Actually that's not too heavy.")
+print("He's got %s eyes and %s hair."% (my_eyes, my_hair) )
+print("His teeth are usually %s depending on the coffee." % my_teeth)
+
+#this line is tricky, try to get it exactly right.
+print("If I add %d, %d,and %d I get %d." %(my_age,my_height,my_weight,my_age+my_height+my_weight) )
+
+#ex6
+x="There are %d types of people" %10
+binary="binary"
+do_not="don't"
+y="Those who know %s and those who %s" %(binary,do_not)
+
+print (x)
+print (y)
+
+print("I said: %r"%x)
+print("I also said:'%s'"%y)
+hilarious=False
+joke_evaluation="Isn't that joke so funny?!%r"
+
+print(joke_evaluation % hilarious)
+
+w="This is the left side of..."
+e="a string with a right side"
+
+print(w+e)
