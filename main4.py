ad1870a49e76ad6ac45a772144f55c80408ad7e2
SemraKilinc <60199182+SemraKilinc@users.noreply.github.com>
2020-03-02 15:30:50 -0600

    Add files via upload

---
+++ main4.py
@@ -0,0 +1,27 @@
+list1=("1","2","3","4")
+list2=("5","6","7","8")
+list3=list1+list2
+print(list3)
+
+a=12
+for i in range(1,15):
+  print(a,"X",i,"=", a*i)
+
+for i in range(1,101):
+  if (i % 2==0): 
+    
+  
+
+
+
+
+
+
+
+#3
+answer=""
+while(answer!="y/n"):
+  answer= input ("Do you feel okay? (y/n)")
+  print("No")
+
+
---
+++ main5.py
@@ -0,0 +1,9 @@
+x=5
+while(x>=0):
+  x -= 0.5
+  print(x)
+
+x=2
+while(x<=5):
+  x = x**2
+  print (x)
